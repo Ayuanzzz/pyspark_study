4db6041e5597c2310c742ae687617545270894e1
Ayuanzzz <1052833858@qq.com>
2026-05-10 09:20:35 +0800

    Add study script

---
+++ study.py
@@ -0,0 +1,8 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("StudyApp").getOrCreate()
+
+textFile = spark.read.text("README.md")
+
+print(textFile.count())
+print(textFile.first())
