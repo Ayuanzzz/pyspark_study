9baa5c2b1c5fa4158cfa9f65d31d976e88ea3b78
Ayuanzzz <1052833858@qq.com>
2026-05-10 15:42:38 +0800

    Update study.py

--- study.py
+++ study.py
@@ -2,9 +2,9 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("StudyApp").getOrCreate()
 
+# create branch studydatabrciks
+
 textFile = spark.read.text("README.md")
 
 print(textFile.count())
 print(textFile.first())
-
-# create branch study
